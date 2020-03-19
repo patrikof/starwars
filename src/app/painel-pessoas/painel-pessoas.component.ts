f9b7d2bceed5866e4329abf0edec3c6cab0ebc7f
Patrik de Oliveira França <patrikoliveira@yahoo.com.br>
2020-03-18 22:05:45 -0300

    carregando resultados novos nos botões

--- src/app/painel-pessoas/painel-pessoas.component.ts
+++ src/app/painel-pessoas/painel-pessoas.component.ts
@@ -2,10 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { PessoaService } from '../pessoa.service';
 import { People } from '../people';
 import { PlanetaService } from '../planeta.service';
-import { Planets } from '../planets';
 import { Pessoa } from '../results';
 import { RacaService } from '../raca.service';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-painel-pessoas',
@@ -32,28 +31,7 @@ export class PainelPessoasComponent implements OnInit {
         this.pessoas = resposta;
         this.urlAnterior = this.pessoas.previous;
         this.urlProxima = this.pessoas.next;
-
-        this.resultados = new Array<Pessoa>();
-        this.pessoas.results.forEach(p => {
-          let pessoa: Pessoa = new Pessoa();
-          pessoa.nome = p.name;
-          pessoa.genero = p.gender;
-          pessoa.nascimento = p.birth_year;
-
-          if (p.homeworld) {
-            pessoa.planeta =  this.planetaService.getPlaneta(p.homeworld);
-          }
-          else {
-            pessoa.planeta = of("Unknow");
-          }
-          if (p.species[0]) {
-            pessoa.raca = this.racaService.getRaca(p.species[0]);
-          }
-          else {
-            pessoa.planeta = of("Unknow");
-          }
-          this.resultados.push(pessoa);
-        });
+        this.carregarResultados();        
       });
   }
 
@@ -63,6 +41,7 @@ export class PainelPessoasComponent implements OnInit {
         this.pessoas = resposta
         this.urlAnterior = this.pessoas.previous;
         this.urlProxima = this.pessoas.next;
+        this.carregarResultados();
       });
   }
 
@@ -72,10 +51,11 @@ export class PainelPessoasComponent implements OnInit {
         this.pessoas = resposta;
         this.urlAnterior = this.pessoas.previous;
         this.urlProxima = this.pessoas.next;
+        this.carregarResultados();
       });
   }
 
-  /*getPlaneta(urlPlaneta: string) {
+  getPlaneta(urlPlaneta: string) {
     console.log("iniciando o get do planeta");
     let planeta: string;
     this.planetaService.getPlaneta(urlPlaneta).subscribe(resposta => planeta = resposta.name)
@@ -87,5 +67,29 @@ export class PainelPessoasComponent implements OnInit {
     let raca: string;
     this.racaService.getRaca(urlRaca).subscribe(resposta => raca = resposta.name)
     return raca;
-  }*/
+  }
+
+  carregarResultados(){
+    this.resultados = new Array<Pessoa>();
+        this.pessoas.results.forEach(p => {
+          let pessoa: Pessoa = new Pessoa();
+          pessoa.nome = p.name;
+          pessoa.genero = p.gender;
+          pessoa.nascimento = p.birth_year;
+
+          if (p.homeworld) {
+            pessoa.planeta = Observable.create(this.getPlaneta(p.homeworld));
+          }
+          else {
+            pessoa.planeta = Observable.create("Unknow");
+          }
+          if (p.species[0]) {
+            pessoa.raca = Observable.create(this.getRaca(p.species[0]));
+          }
+          else {
+            pessoa.planeta = Observable.create("Unknow");
+          }
+          this.resultados.push(pessoa);
+        });      
+  }
 }
